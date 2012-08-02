c03fe9a9203528715cd87c7557b4d2242a9433fd
Rick Briganti <jeviolle@gmail.com>
2012-08-01 20:16:23 -0400

    Added populateDB.php and lsi.sql for storing lsi results into MySQL

---
+++ src/bin/populateDB.php
@@ -0,0 +1,185 @@
+#!/usr/bin/env php
+<?php
+#
+# lsiquery.php - script to query and ouput stored data
+#
+#    lsi - A collection of scripts and programs that allow for the gathering
+#    and reporting of Linux/Unix systems information.
+#
+#    Copyright (C) 2012, Rick Briganti
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# source and connect to the database
+include ('includes/db-connect.inc');
+
+# set timezone
+date_default_timezone_set('America/New_York');
+
+# generic function for error handling
+function myerr() {
+  echo "MySQL Error " . mysql_errno() . " : " . mysql_error() . "\n";
+}
+
+class lsi {
+    function biosinfo($host,$systeminfo) {
+        $myDate = date("Y-m-d H:i:s", strtotime($systeminfo->bios->date));
+        $pBios="CALL pBios(\"$host\",\"{$systeminfo->bios->product}\",\"{$systeminfo->bios->revision}\",\"$myDate\",\"{$systeminfo->bios->hostid}\")";
+
+        if(!mysql_query($pBios)) { myerr(); }
+    }
+
+    function cpuinfo($host,$systeminfo) {
+    
+        $pCPUCount="CALL pCPUCount(\"$host\",\"{$systeminfo->cpus->count}\")";
+        if(!mysql_query($pCPUCount)) { myerr(); }
+
+        $cleanCPU="DELETE FROM processors WHERE id=(SELECT id FROM hosts WHERE hostname=\"$host\");";
+        if(!mysql_query($cleanCPU)) { 
+	  myerr();
+        } else {
+          foreach ($systeminfo->cpus->cpu as $cpu) {
+            $pCPU="CALL pCpu(\"$host\",\"$cpu->vendor_id\",\"$cpu->model_name\",\"$cpu->cpu_MHz\",\"$cpu->cache_size\")";
+            if(!mysql_query($pCPU)) { myerr(); }
+          }
+        }
+    }
+
+    function diskinfo($host,$systeminfo) {
+
+        $pDiskCount="CALL pDiskCount(\"$host\",\"{$systeminfo->disks->count}\")";
+        if (!mysql_query($pDiskCount)) { myerr(); }
+
+        $cleanDisk="DELETE FROM disks WHERE id=(SELECT id FROM hosts WHERE hostname=\"$host\");";
+        if(!mysql_query($cleanDisk)) { 
+	  myerr();
+	} else {
+          foreach ($systeminfo->disks->disk as $disk) {
+            $pDisk="CALL pDisk(\"$host\",\"$disk->device\",\"$disk->size\",\"$disk->model\",\"$disk->interface\")";
+            if (!mysql_query($pDisk)) { myerr(); }
+          }
+        }
+    }
+
+    function hbainfo($host,$systeminfo) {
+        $pHBACount="CALL pHBACount(\"$host\",\"{$systeminfo->hbas->count}\")";
+	if (!mysql_query($pHBACount)) { myerr(); }
+
+        $cleanHba="DELETE FROM hba_info WHERE id=(SELECT id FROM hosts WHERE hostname=\"$host\");";
+        if (!mysql_query($cleanHba)) {
+          myerr();
+        } else {
+          $pHba="CALL pHba(\"$host\",\"{$systeminfo->hbas->description}\")";
+	  if (!mysql_query($pHba)) { myerr(); }
+        }
+    }
+
+    function hostinfo($host,$systeminfo) {
+        $pHost="CALL pHost(\"$host\",\"{$systeminfo->host->vendor}\",\"{$systeminfo->host->version}\",\"{$systeminfo->host->kernel}\",\"{$systeminfo->host->arch}\",\"{$systeminfo->host->numa}\")";
+        if (!mysql_query($pHost)) { myerr(); }
+        
+    }
+
+    function meminfo($host,$systeminfo) {
+
+        $pMem="CALL pMem(\"$host\",\"{$systeminfo->memory->installed}\",\"{$systeminfo->memory->maximum}\",\"{$systeminfo->memory->mbperslot}\",\"{$systeminfo->memory->slots}\")";
+	if (!mysql_query($pMem)) { myerr(); }
+
+        $cleanDimm="DELETE FROM memory_info WHERE id=(SELECT id FROM hosts WHERE hostname=\"$host\");";
+        if (!mysql_query($cleanDimm)) {
+          myerr();
+        } else {
+          foreach ($systeminfo->memory->dimm as $dimm) {
+            $pDimm="CALL pDimm(\"$host\",\"$dimm->form\",\"$dimm->locator\",\"$dimm->size\",\"$dimm->speed\")";
+	    if (!mysql_query($pDimm)) { myerr(); }
+          }
+        }
+    }
+
+    function partinfo($host,$systeminfo) {
+
+        $pPartCount="CALL pPartCount(\"$host\",\"{$systeminfo->partitions->count}\")";
+	if (!mysql_query($pPartCount)) { myerr(); }
+
+        $cleanPart="DELETE FROM partitions WHERE id=(SELECT id FROM hosts WHERE hostname=\"$host\");";
+        if (!mysql_query($cleanPart)) {
+	  myerr();
+	} else {
+          foreach ($systeminfo->partitions->partition as $partition) {
+            $pPart="CALL pPart(\"$host\",\"$partition->device\",\"$partition->filesystem\",\"$partition->mount\",\"$partition->options\")";
+	    if (!mysql_query($pPart)) { myerr(); }
+          }
+        }
+    }
+
+    function pkginfo($host,$systeminfo) {
+        $cleanHostPackages="DELETE FROM hostPackages WHERE hostID=(SELECT id FROM hosts WHERE hostname=\"$host\");";
+        if (!mysql_query($cleanHostPackages)) {
+          myerr();
+        } else {
+          foreach ($systeminfo->packages->package as $package) {
+	    # escape strings since descriptions and maintainer stuff might contain quotes etcs
+            $desc=mysql_real_escape_string($package->description);
+            $maint=mysql_real_escape_string($package->maintainer);
+	    
+            $pPkg="CALL pPkg(\"$host\",\"$package->package\",\"$package->version\",\"$package->release\",\"$package->architecture\",\"$package->installdate\",\"$maint\",\"$desc\",\"$package->status\")";
+	    if (!mysql_query($pPkg)) { 
+	        myerr(); 
+            }
+          }
+        }
+    }
+
+    function nicinfo($host,$systeminfo) {
+        $pNicCount="CALL pNicCount(\"$host\",\"{$systeminfo->nics->count}\")";
+        if (!mysql_query($pNicCount)) { myerr(); }
+
+	$cleanNic="DELETE FROM nic_info WHERE id=(SELECT id FROM hosts WHERE hostname=\"$host\");";
+        if (!mysql_query($cleanNic)) {
+	  myerr();
+	} else {
+          foreach ($systeminfo->nics->nic as $nic) {
+            $pNic="CALL pNic(\"$host\",\"$nic->device\",\"$nic->ip\",\"$nic->netmask\",\"$nic->mac\")";
+            if (!mysql_query($pNic)) { myerr(); }
+          }
+	}
+    }
+}
+
+# search all xml data
+$command = 'find ./data -type f';
+exec($command, $xmlfiles);
+foreach($xmlfiles as $file){
+    # read xml data
+    $xmldata = file_get_contents($file);
+    $systeminfo = new SimpleXMLElement($xmldata);
+
+    #print_r($systeminfo);
+    $host=$systeminfo['host'];
+
+    # create new class instance
+    $lsi = new lsi();
+
+    $lsi->hostinfo($host,$systeminfo);
+    $lsi->biosinfo($host,$systeminfo);
+    $lsi->cpuinfo($host,$systeminfo);
+    $lsi->meminfo($host,$systeminfo);
+    $lsi->diskinfo($host,$systeminfo);
+    $lsi->nicinfo($host,$systeminfo);
+    $lsi->hbainfo($host,$systeminfo);
+    $lsi->partinfo($host,$systeminfo);
+    $lsi->pkginfo($host,$systeminfo);
+}
+?>
---
+++ src/share/doc/lsi.sql
@@ -0,0 +1,541 @@
+--#
+--# lsi.sql
+--#
+--#    lsi - A collection of scripts and programs that allow for the gathering
+--#    and reporting of Linux/Unix systems information.
+--#
+--#    Copyright (C) 2012, Rick Briganti
+--#
+--#    This program is free software: you can redistribute it and/or modify
+--#    it under the terms of the GNU General Public License as published by
+--#    the Free Software Foundation, either version 3 of the License, or
+--#    (at your option) any later version.
+--#
+--#    This program is distributed in the hope that it will be useful,
+--#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--#    GNU General Public License for more details.
+--#
+--#    You should have received a copy of the GNU General Public License
+--#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--#
+--
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+DROP SCHEMA IF EXISTS `lsi` ;
+CREATE SCHEMA IF NOT EXISTS `lsi` DEFAULT CHARACTER SET latin1 ;
+USE `lsi` ;
+
+-- -----------------------------------------------------
+-- Table `lsi`.`hosts`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `lsi`.`hosts` ;
+
+CREATE  TABLE IF NOT EXISTS `lsi`.`hosts` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `hostname` VARCHAR(255) NOT NULL ,
+  `architecture` VARCHAR(10) NOT NULL ,
+  `kernel` VARCHAR(45) NOT NULL ,
+  `numa` TINYINT(1) NOT NULL ,
+  `vendor` VARCHAR(30) NOT NULL ,
+  `version` VARCHAR(30) NOT NULL ,
+  `cpu_count` INT(11) NULL ,
+  `part_count` INT(11) NULL ,
+  `disk_count` INT(11) NULL ,
+  `hba_count` INT(11) NULL ,
+  `nic_count` INT(11) NULL ,
+  `pkg_count` INT(11) NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `hostname` (`hostname` ASC) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `lsi`.`bios_info`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `lsi`.`bios_info` ;
+
+CREATE  TABLE IF NOT EXISTS `lsi`.`bios_info` (
+  `id` INT NOT NULL ,
+  `hostid` VARCHAR(30) NOT NULL ,
+  `product` VARCHAR(50) NOT NULL ,
+  `biosrev` CHAR(30) NOT NULL ,
+  `date` DATE NOT NULL ,
+  `memory_slots` INT(11) NULL ,
+  `maximum_mem` INT(30) NULL ,
+  `installed_mem` VARCHAR(45) NULL ,
+  `mbperslot` INT(11) NULL ,
+  INDEX `hostid` (`hostid` ASC) ,
+  INDEX `fk_bios_hostId` (`id` ASC) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `lsi`.`disks`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `lsi`.`disks` ;
+
+CREATE  TABLE IF NOT EXISTS `lsi`.`disks` (
+  `id` INT NOT NULL ,
+  `device` VARCHAR(30) NOT NULL ,
+  `interface` VARCHAR(10) NOT NULL ,
+  `model` VARCHAR(30) NOT NULL ,
+  `size` VARCHAR(15) NOT NULL ,
+  INDEX `fk_disk_hostId` (`id` ASC) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `lsi`.`hba_info`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `lsi`.`hba_info` ;
+
+CREATE  TABLE IF NOT EXISTS `lsi`.`hba_info` (
+  `id` INT NOT NULL ,
+  `description` VARCHAR(200) NOT NULL ,
+  INDEX `fk_hba_hostId` (`id` ASC) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `lsi`.`memory_info`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `lsi`.`memory_info` ;
+
+CREATE  TABLE IF NOT EXISTS `lsi`.`memory_info` (
+  `id` INT NOT NULL ,
+  `form` VARCHAR(10) NOT NULL ,
+  `locator` VARCHAR(10) NOT NULL ,
+  `size` VARCHAR(30) NOT NULL ,
+  `speed` VARCHAR(10) NOT NULL ,
+  INDEX `form` (`form` ASC) ,
+  INDEX `locator` (`locator` ASC) ,
+  INDEX `size` (`size` ASC) ,
+  INDEX `speed` (`speed` ASC) ,
+  INDEX `fk_mem_hostId` (`id` ASC) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `lsi`.`nic_info`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `lsi`.`nic_info` ;
+
+CREATE  TABLE IF NOT EXISTS `lsi`.`nic_info` (
+  `id` INT NOT NULL ,
+  `device` VARCHAR(10) NOT NULL ,
+  `ipaddr` VARCHAR(255) NOT NULL ,
+  `macaddr` VARCHAR(255) NOT NULL ,
+  `netmask` VARCHAR(255) NOT NULL ,
+  INDEX `fk_nic_hostId` (`id` ASC) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `lsi`.`packages`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `lsi`.`packages` ;
+
+CREATE  TABLE IF NOT EXISTS `lsi`.`packages` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `package` VARCHAR(255) NOT NULL ,
+  `maintainer` VARCHAR(100) NOT NULL ,
+  `description` VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `id` (`id` ASC) ,
+  INDEX `package` (`package` ASC) ,
+  INDEX `maintainer` (`maintainer` ASC) ,
+  INDEX `description` (`description` ASC) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `lsi`.`partitions`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `lsi`.`partitions` ;
+
+CREATE  TABLE IF NOT EXISTS `lsi`.`partitions` (
+  `id` INT NOT NULL ,
+  `device` VARCHAR(50) NOT NULL ,
+  `filesystem` VARCHAR(10) NOT NULL ,
+  `mount` VARCHAR(255) NOT NULL ,
+  `options` VARCHAR(255) NOT NULL ,
+  INDEX `device` (`device` ASC) ,
+  INDEX `filesystem` (`filesystem` ASC) ,
+  INDEX `mount` (`mount` ASC) ,
+  INDEX `options` (`options` ASC) ,
+  INDEX `fk_part_hostId` (`id` ASC) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `lsi`.`processors`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `lsi`.`processors` ;
+
+CREATE  TABLE IF NOT EXISTS `lsi`.`processors` (
+  `id` INT NOT NULL ,
+  `cache` VARCHAR(10) NOT NULL ,
+  `speed` VARCHAR(10) NOT NULL ,
+  `model` VARCHAR(50) NOT NULL ,
+  `vendor` VARCHAR(30) NOT NULL ,
+  INDEX `cache` (`cache` ASC) ,
+  INDEX `speed` (`speed` ASC) ,
+  INDEX `model` (`model` ASC) ,
+  INDEX `fk_proc_id` (`id` ASC) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `lsi`.`hostPackages`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `lsi`.`hostPackages` ;
+
+CREATE  TABLE IF NOT EXISTS `lsi`.`hostPackages` (
+  `hostID` INT NOT NULL ,
+  `packageID` INT NOT NULL ,
+  `installDate` TIMESTAMP NOT NULL ,
+  `status` VARCHAR(45) NULL ,
+  INDEX `fk_hostID` (`hostID` ASC) ,
+  INDEX `fk_pkgID` (`packageID` ASC) ,
+  PRIMARY KEY (`hostID`, `packageID`) )
+ENGINE = MyISAM;
+
+
+-- -----------------------------------------------------
+-- procedure pBios
+-- -----------------------------------------------------
+
+USE `lsi`;
+DROP procedure IF EXISTS `lsi`.`pBios`;
+
+DELIMITER $$
+USE `lsi`$$
+CREATE PROCEDURE `lsi`.`pBios` (host varchar(255),product varchar(50),biosrev varchar(30),date date,hostid varchar(30))
+BEGIN
+  IF EXISTS (SELECT id FROM bios_info WHERE id=(SELECT id FROM hosts WHERE hostname=host)) THEN
+    UPDATE bios_info SET hostid=hostid,product=product,biosrev=biosrev,date=date WHERE id=(SELECT id FROM hosts WHERE hostname=host);
+  ELSE
+    INSERT INTO bios_info (id,hostid,product,biosrev,date) VALUES ((SELECT id FROM hosts WHERE hostname=host),hostid,biosrev,product,date); 
+  END IF;
+END 
+$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure pCPUCount
+-- -----------------------------------------------------
+
+USE `lsi`;
+DROP procedure IF EXISTS `lsi`.`pCPUCount`;
+
+DELIMITER $$
+USE `lsi`$$
+CREATE PROCEDURE `lsi`.`pCPUCount` (host varchar(255),count INT(11))
+BEGIN
+  IF EXISTS (SELECT id FROM hosts WHERE hostname=host) THEN
+    UPDATE hosts SET cpu_count=count WHERE hostname=host;
+  END IF;
+END $$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure pCpu
+-- -----------------------------------------------------
+
+USE `lsi`;
+DROP procedure IF EXISTS `lsi`.`pCpu`;
+
+DELIMITER $$
+USE `lsi`$$
+CREATE PROCEDURE `lsi`.`pCpu` (host VARCHAR(255),vendor VARCHAR(30),model VARCHAR(50),speed VARCHAR(10),cache VARCHAR(10))
+BEGIN
+INSERT INTO processors (id,vendor,model,speed,cache) VALUES ((SELECT id FROM hosts WHERE hostname=host),vendor,model,speed,cache);
+END $$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure pDiskCount
+-- -----------------------------------------------------
+
+USE `lsi`;
+DROP procedure IF EXISTS `lsi`.`pDiskCount`;
+
+DELIMITER $$
+USE `lsi`$$
+CREATE PROCEDURE `lsi`.`pDiskCount` (host VARCHAR(255),count INT(11))
+BEGIN
+  IF EXISTS (SELECT id FROM hosts WHERE hostname=host) THEN
+    UPDATE hosts SET disk_count=count WHERE hostname=host; 
+  END IF;
+END $$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure pDisk
+-- -----------------------------------------------------
+
+USE `lsi`;
+DROP procedure IF EXISTS `lsi`.`pDisk`;
+
+DELIMITER $$
+USE `lsi`$$
+CREATE PROCEDURE `lsi`.`pDisk` (host VARCHAR(255),device VARCHAR(30),size VARCHAR(15),model VARCHAR(30),interface VARCHAR(10))
+BEGIN
+INSERT INTO disks (id,device,size,model,interface) VALUES ((SELECT id FROM hosts WHERE hostname=host),device,size,model,interface); 
+END $$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure pHBACount
+-- -----------------------------------------------------
+
+USE `lsi`;
+DROP procedure IF EXISTS `lsi`.`pHBACount`;
+
+DELIMITER $$
+USE `lsi`$$
+CREATE PROCEDURE `lsi`.`pHBACount` (host VARCHAR(255), count INT(11))
+BEGIN
+  IF EXISTS (SELECT id FROM hosts WHERE hostname=host) THEN
+    UPDATE hosts SET hba_count=count WHERE hostname=host; 
+  END IF; 
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure pHba
+-- -----------------------------------------------------
+
+USE `lsi`;
+DROP procedure IF EXISTS `lsi`.`pHba`;
+
+DELIMITER $$
+USE `lsi`$$
+CREATE PROCEDURE `lsi`.`pHba` (host VARCHAR(255),description VARCHAR(200))
+BEGIN
+SET @vDescription:=description;
+  IF (SELECT @vDescription != " ") THEN
+    INSERT INTO hba_info (id,description) VALUES ((SELECT id FROM hosts WHERE hostname=host),description); 
+  END IF;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure pHost
+-- -----------------------------------------------------
+
+USE `lsi`;
+DROP procedure IF EXISTS `lsi`.`pHost`;
+
+DELIMITER $$
+USE `lsi`$$
+CREATE PROCEDURE `lsi`.`pHost` (host VARCHAR(255),vendor VARCHAR(30),version VARCHAR(30),kernel VARCHAR(45),arch VARCHAR(10),numa TINYINT(1))
+BEGIN
+  IF EXISTS (SELECT id FROM hosts WHERE hostname=host) THEN
+    UPDATE hosts SET vendor=vendor,version=version,kernel=kernel,architecture=architecture,numa=numa WHERE hostname=host; 
+  ELSE
+    INSERT INTO hosts (hostname,vendor,version,kernel,architecture,numa) VALUES (host,vendor,version,kernel,arch,numa); 
+  END IF;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure pMem
+-- -----------------------------------------------------
+
+USE `lsi`;
+DROP procedure IF EXISTS `lsi`.`pMem`;
+
+DELIMITER $$
+USE `lsi`$$
+CREATE PROCEDURE `lsi`.`pMem` (host VARCHAR(255),installed VARCHAR(45), max INT(30), mbperslot INT(11),slots INT(11))
+BEGIN
+  IF EXISTS (SELECT id FROM bios_info WHERE id=(SELECT id FROM hosts WHERE hostname=host)) THEN
+    UPDATE bios_info SET installed_mem=installed,maximum_mem=max,mbperslot=mbperslot,memory_slots=slots WHERE id=(SELECT id FROM hosts WHERE hostname=host);
+  END IF;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure pDimm
+-- -----------------------------------------------------
+
+USE `lsi`;
+DROP procedure IF EXISTS `lsi`.`pDimm`;
+
+DELIMITER $$
+USE `lsi`$$
+CREATE PROCEDURE `lsi`.`pDimm` (host VARCHAR(255),form VARCHAR(10),locator VARCHAR(10),size VARCHAR(30),speed VARCHAR(10))
+BEGIN
+INSERT INTO memory_info (id,form,locator,size,speed) VALUES ((SELECT id FROM hosts WHERE hostname=host),form,locator,size,speed);
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure pPartCount
+-- -----------------------------------------------------
+
+USE `lsi`;
+DROP procedure IF EXISTS `lsi`.`pPartCount`;
+
+DELIMITER $$
+USE `lsi`$$
+CREATE PROCEDURE `lsi`.`pPartCount` (host VARCHAR(255),count INT(11))
+BEGIN
+  IF EXISTS (SELECT id FROM hosts WHERE hostname=host) THEN
+    UPDATE hosts SET part_count=count WHERE hostname=host; 
+  END IF;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure pPart
+-- -----------------------------------------------------
+
+USE `lsi`;
+DROP procedure IF EXISTS `lsi`.`pPart`;
+
+DELIMITER $$
+USE `lsi`$$
+CREATE PROCEDURE `lsi`.`pPart` (host VARCHAR(255),device VARCHAR(50),fs VARCHAR(10),mount VARCHAR(255),options VARCHAR(255))
+BEGIN
+INSERT INTO partitions (id,device,filesystem,mount,options) VALUES ((SELECT id FROM hosts WHERE hostname=host),device,fs,mount,options);
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure pPkg
+-- -----------------------------------------------------
+
+USE `lsi`;
+DROP procedure IF EXISTS `lsi`.`pPkg`;
+
+DELIMITER $$
+USE `lsi`$$
+CREATE PROCEDURE `lsi`.`pPkg` (host VARCHAR(255),name VARCHAR(100),version VARCHAR(30),revision VARCHAR(30),architecture VARCHAR(10),installdate TIMESTAMP,maintainer VARCHAR(100), description VARCHAR(255),status VARCHAR(45))
+BEGIN
+  # get host id
+  SELECT id INTO @hostid FROM hosts WHERE hostname=host;
+  
+  # Generate package name based on the type
+  # RPM do not have a installed status
+  IF (status = " ") THEN
+    SET @pkgname = concat(name,"-",version,"-",revision,".",architecture);
+  ELSE
+    SET @pkgname = concat(name,"_",version,"-",revision,"_",architecture);
+  END IF;
+
+  SELECT count(*) INTO @pkgcount FROM packages 
+  WHERE package=@pkgname 
+  AND maintainer=maintainer 
+  AND description=description;
+    
+  IF (@pkgcount = 0) THEN
+    INSERT INTO packages (package,maintainer,description) VALUES (
+    @pkgname,
+    maintainer,
+    description);
+  END IF;
+
+  # populate host pacakages to map packages to hosts
+  SELECT id INTO @pkgid FROM packages WHERE package=@pkgname AND maintainer=maintainer AND description=description;
+  INSERT IGNORE INTO hostPackages (hostID,packageID,installDate,status) VALUES (@hostid,@pkgid,installdate,status);  
+
+  # update the package count from the host packages table
+  UPDATE hosts SET pkg_count=(SELECT count(*) FROM hostPackages WHERE hostID=@hostid) WHERE id=@hostid; 
+
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure pNicCount
+-- -----------------------------------------------------
+
+USE `lsi`;
+DROP procedure IF EXISTS `lsi`.`pNicCount`;
+
+DELIMITER $$
+USE `lsi`$$
+CREATE PROCEDURE `lsi`.`pNicCount` (host VARCHAR(255),count INT(11))
+BEGIN
+  IF EXISTS (SELECT id FROM hosts WHERE hostname=host) THEN
+    UPDATE hosts SET nic_count=count WHERE hostname=host; 
+  END IF;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure pNic
+-- -----------------------------------------------------
+
+USE `lsi`;
+DROP procedure IF EXISTS `lsi`.`pNic`;
+
+DELIMITER $$
+USE `lsi`$$
+CREATE PROCEDURE `lsi`.`pNic` (host VARCHAR(255),device VARCHAR(10),ipaddr VARCHAR(255),netmask VARCHAR(255),macaddr VARCHAR(255))
+BEGIN
+INSERT INTO nic_info (id,device,ipaddr,netmask,macaddr) VALUES ((SELECT id FROM hosts WHERE hostname=host),device,ipaddr,netmask,macaddr); 
+END $$
+
+DELIMITER ;
+USE `lsi`;
+
+DELIMITER $$
+
+USE `lsi`$$
+DROP TRIGGER IF EXISTS `lsi`.`bd_host` $$
+USE `lsi`$$
+
+
+# automatically delete on host dependent information
+# in all tables
+CREATE TRIGGER bd_host
+BEFORE DELETE on hosts
+FOR EACH ROW
+BEGIN
+  DELETE FROM bios_info WHERE bios_info.id=OLD.id;
+  DELETE FROM disks WHERE disks.id=OLD.id;
+  DELETE FROM memory_info WHERE memory_info.id=OLD.id;
+  DELETE FROM processors WHERE processors.id=OLD.id;
+  DELETE FROM hba_info WHERE hba_info.id=OLD.id;
+  DELETE FROM hostPackages WHERE hostPackages.hostID=OLD.id;
+  DELETE FROM nic_info WHERE nic_info.id=OLD.id;
+  DELETE FROM partitions WHERE partitions.id=OLD.id;
+END
+$$
+
+
+DELIMITER ;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
